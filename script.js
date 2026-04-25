1e9fc21714d769fdac09bedaf96d61a12d9e2b62
N.Sri Venkatesh <srivenkateshnurukurthi@gmail.com>
2026-04-25 11:58:57 +0530

    temp: save dark mode fixes before switching

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const toggleBtn = document.getElementById("theme-toggle");
 
 if (toggleBtn) {
   toggleBtn.addEventListener("click", () => {
-    document.body.classList.toggle("dark-theme");
+    document.body.classList.toggle("dark-mode");
   });
 }
 if (yearElement) {
