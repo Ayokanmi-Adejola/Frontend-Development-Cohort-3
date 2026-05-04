02bec37c571e98b51e9fa763dd2723601a3e213a
Ayokanmi Adejola <adejolaayokanmi90@gmail.com>
2026-05-04 14:48:42 +0100

    Update form.js

--- form.js
+++ form.js
@@ -5,14 +5,14 @@ const form = document.getElementById("form");
 
 form.addEventListener("submit", (e) => {
   // Add your form validation and submission laogic here
-  /*if (
+  if (
     name.value === "" ||
     email.value === "" ||
     password.value === "" ||
     confirmPassword.value === ""
   ) { 
-    alert("please fill in all required fields.");*/
-  if (password.value.length < 8) {
+    alert("please fill in all required fields.");
+  /* if (password.value.length < 8) {
     alert("Password must be at least 8 characters long");
   }
   if (password.value === "password") {
@@ -20,5 +20,5 @@ form.addEventListener("submit", (e) => {
   }
 
   e.preventDefault();
-  validateform();
+  validateform(); */
 });
