65b31c5b60b2bcd883a376b1294f8b08e9bf92dd
unknown <aniyikayeademola8@gmail.com>
2026-07-12 04:11:59 -0700

    click me functionality

--- form.js
+++ form.js
@@ -5,19 +5,27 @@ const form = document.getElementById("form");
 
 form.addEventListener("submit", (e) => {
   // Add your form validation and submission laogic here
-  if (
-    name.value === "" ||
-    email.value === "" ||
-    password.value === "" 
-  ) { 
+  if (name.value === "" || 
+    email.value === "" || 
+    password.value === "") {
     alert("please fill in all required fields.");
-  /* if (password.value.length < 8) {
-    alert("Password must be at least 8 characters long");
-  }
-  if (password.value === "password") {
-    alert("Password cannot be password");
-  }
+    /* if (password.value.length < 8) {
+      alert("Password must be at least 8 characters long");
+    }
+    if (password.value === "password") {
+      alert("Password cannot be password");
+    }
 
-  e.preventDefault();
-  validateform(); */
+    e.preventDefault();
+    validateform(); */
+  }
 });
+
+const clickButton = document.querySelector("#click-btn");
+const frontendParagraph = document.getElementById("frontend");
+
+if (clickButton && frontendParagraph) {
+  clickButton.addEventListener("click", () => {
+    frontendParagraph.textContent = "Hello Cohort 3! The button works!";
+  });
+}
